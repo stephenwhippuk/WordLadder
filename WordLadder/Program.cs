99f91625284c7bbc3fb8e1ae4262dd27ca6d217b
Stephen Whipp <stephenwhippuk@gmail.com>
2019-09-04 21:51:11 +0100

    Add project files.

---
+++ WordLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WordLadderAPI/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WordLadderAPI
+{
+    public abstract class IWordSequence
+    {
+        public int Length { get; set; }
+
+        public abstract override string ToString() ;
+    }
+}
