bcba62aa2ca55694780b15dd8419d492781dd5b6
Stephen Whipp <stephenwhippuk@gmail.com>
2019-09-12 03:57:45 +0100

    Tidied up demonstration program

--- WordLadder/Program.cs
+++ WordLadder/Program.cs
@@ -16,17 +16,40 @@ namespace WordLadder
             List<IWordNode> pool = new List<IWordNode>();
 
             ShortestSequenceCalculator<WordSequence, List<IWordNode>> calculator = new ShortestSequenceCalculator<WordSequence, List<IWordNode>> { Start = first, Finish = last, WordPool = pool };
-            IWordReader reader = new FileWordReader<WordNode>(args[2]);
-            reader.Open();
-            calculator.Load(reader);
+            try
+            {
+                Console.WriteLine("\nStart Word is \"" + args[0] + "\"");
+                Console.WriteLine("Finish Word is \"" + args[1] + "\"");
+                Console.WriteLine("Input File Path is \"" + args[2] + "\"");
+                Console.WriteLine("Output File Path is \"" + args[3] + "\"");
 
-            WordSequence shortest = new WordSequence();
-            calculator.GetPath(shortest);
+                Console.WriteLine("\nLoading Word Pool from input file...");
+                IWordReader reader = new FileWordReader<WordNode>(args[2]);
+                reader.Open();
+                calculator.Load(reader);
+                reader.Close();
 
-            var writer = new FileWordWriter(args[3]);
-            writer.Open();
-            shortest.Save(writer);
-            writer.Close();
+                Console.WriteLine("Calculating Shortest Path...");
+                WordSequence shortest = new WordSequence();
+                calculator.GetPath(shortest);
+
+                Console.WriteLine("Saving Results to Output File...");
+                var writer = new FileWordWriter(args[3]);
+                writer.Open();
+                shortest.Save(writer);
+                writer.Close();
+
+                Console.WriteLine("\nThe Shortest Path From \"" + args[0] + "\" to \"" + args[1] + "\" is:");
+                Console.WriteLine("[" + shortest.ToString() + "]");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+               
+            }
+            
+
+           
         }
     }
 }
